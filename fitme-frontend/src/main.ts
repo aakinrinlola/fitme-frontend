7140c7c9bb5b9376cabb08f08ffe5fb37eb96037
aakinrinlola <a.akinrinlola@hotmail.com>
2026-03-23 13:31:29 +0100

    Feedback + History

--- fitme-frontend/src/main.ts
+++ fitme-frontend/src/main.ts
@@ -1,12 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import {importProvidersFrom} from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
 
-bootstrapApplication(App, {
-  providers: [
-    ...appConfig.providers,
-    importProvidersFrom(HttpClientModule)
-  ]
-});
+bootstrapApplication(App, appConfig).catch(err => console.error(err));
